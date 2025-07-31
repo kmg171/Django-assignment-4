eed11ea938d59acfc8646a9a6a02bcd06ec90608
kmg171 <qweaiiok4781@gmail.com>
2025-07-31 17:52:09 +0900

    ✨feat: chapter_04 과제 업로드

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django_extensions',
 ]
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
