66ef6536f010e090dfbf6d9a37473f1a1e665d73
kmg171 <qweaiiok4781@gmail.com>
2025-07-31 17:47:34 +0900

    chapter_01 과제 수정 완료

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_extensions',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
